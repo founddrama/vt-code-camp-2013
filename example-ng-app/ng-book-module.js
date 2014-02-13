793c7cc05a9ce3ae495b2bdc7ab82396b42c0cec
Rob Friesel <robf@dealer.com>
2014-02-12 19:32:31 -0500

    the presentation... as I gave it on 2/12/2014

--- example-ng-app/ng-book-module.js
+++ example-ng-app/ng-book-module.js
@@ -1,46 +1,47 @@
 /*globals angular:true */
+(function(ng) {
+  ng.module('books', []);
 
-angular.module('books', []);
+  ng.module('books').controller('BookListController', [
+    '$scope', '$window',
+    function($scope, $window) {
+      $scope.books = grDataService.getBook();
 
-angular.module('books').controller('BookListController', [
-  '$scope', '$window',
-  function($scope, $window) {
-    $scope.books = $window.BOOKS;
+      $scope.getStarClass = function(rating, compare) {
+        return 'glyphicon glyphicon-star' + ((rating > compare) ? '' : '-empty');
+      };
 
-    $scope.getStarClass = function(rating, compare) {
-      return 'glyphicon glyphicon-star' + ((rating > compare) ? '' : '-empty');
-    };
+      $scope.hideCovers = false;
+      $scope.toggleCovers = function() {
+        $scope.hideCovers = !$scope.hideCovers;
+      };
 
-    $scope.hideCovers = false;
-    $scope.toggleCovers = function() {
-      $scope.hideCovers = !$scope.hideCovers;
-    };
+      $scope.readFilter = false;
+      $scope.toggleReadFilter = function() {
+        $scope.readFilter = !$scope.readFilter;
+      };
 
-    $scope.readFilter = false;
-    $scope.toggleReadFilter = function() {
-      $scope.readFilter = !$scope.readFilter;
-    };
+      $scope.isReadAndFiltered = function(book) {
+        if (!$scope.readFilter) {
+          return true;
+        } else {
+          return book.finished !== null;
+        }
+      };
 
-    $scope.isReadAndFiltered = function(book) {
-      if (!$scope.readFilter) {
-        return true;
-      } else {
-        return book.finished !== null;
-      }
-    };
+      $scope.helpedFilter = false;
+      $scope.toggleHelpedFilter = function() {
+        $scope.helpedFilter = !$scope.helpedFilter;
+      };
 
-    $scope.helpedFilter = false;
-    $scope.toggleHelpedFilter = function() {
-      $scope.helpedFilter = !$scope.helpedFilter;
-    };
-
-    var helpedRx = /Functional|Object\-Oriented|Professional/;
-    $scope.isEditedByMe = function(book) {
-      if (!$scope.helpedFilter) {
-        return true;
-      } else {
-        return helpedRx.test(book.title);
-      }
-    };
-  }
-]);
+      var helpedRx = /Functional|Object\-Oriented|Professional/;
+      $scope.isEditedByMe = function(book) {
+        if (!$scope.helpedFilter) {
+          return true;
+        } else {
+          return helpedRx.test(book.title);
+        }
+      };
+    }
+  ]);
+}(angular));
